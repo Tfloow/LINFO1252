56bc824ffab5a1f1adca2f7e22af43182e451fcd
Tfloow <88899420+Tfloow@users.noreply.github.com>
2023-10-06 22:57:47 +0200

    Sort

--- Inginious/S2/ingi.c
+++ Inginious/S2/ingi.c
@@ -11,62 +11,94 @@ void exchange_pointers(int** a, int** b){
    *b = c;
 }
 
-void sort(void *base, size_t nel, size_t width, int (*compare)(const void *, const void *))
-{
-    // Going to implement the worst sorting algorithm ever
-    void* cur = base; void* track = base; char* new  = malloc(width*nel);
-    memcpy(new, (char*) base, nel); // copy the old in the new
-    void* min; int place; char* tmp = (char*) malloc(width);
+void sort(void *base, size_t nel, size_t width, int (*compare)(const void *, const void *)){
+    // Home made bubble sort
+    char* test = (char*) malloc(width*nel);
+    strcpy(test, (char*) base);
+    // Check for error
+    if(nel == 0 || width == 0 || base == NULL){
+        return;
+    } 
 
-    for(int i = 0; i < nel; i++){
-        // so we start with the first element of the non sorted subarray
-        min = track; // the minimum at the start is the first element
-        printf("%s \t %s\n", (char*)min, (char*) cur);
-        for(int j = i; j < nel; j++){
-            if(compare(min, cur) > 0){
-                min = cur;
-                place = j;
-            }
-            cur = cur + width;
-        }
-        memcpy(tmp, min, width);
-        new[i] = *tmp;
-
-        printf("%c \t %c \t %s\n", *tmp,*((char*) min), new);
-        track = (void*)track + width;
-        cur = track;
+    // Init my variables and copy the array
+    void* max; void* track;
+    void* sorted = malloc(nel*width);
+    void* tmpStorage = malloc(width);
+    if(memcpy(sorted, base, nel) == NULL){
+        return;
     }
-    memcpy(base, (void*) new, nel);
+    printf("%s\n", (char*) sorted);
+
+    for(int i = 1; i < nel; i++){
+
+        max = sorted;
+        track = sorted+width;
+
+        for(int j = 1; j < nel - 1; j++){
+            printf("max: %c \t track: %c \t %s\n", *((char*) max), *((char*) track), (char*) sorted);
+            // If our max is still the max compared to the next one
+            if(compare(max, track) > 0){
+                printf("%c is bigger than %c", *((char*) max), *((char*) track));
+                memcpy(tmpStorage, max, width);
+                memcpy(max, track, width);
+                memcpy(track, tmpStorage, width);
+
+            }
+            max = track;
+            track = max+width;
+        }
+    }
+
+    printf("%s\n", (char*) sorted);
+
+    memcpy(base, sorted, nel*width);
+    printf("Copy\n");
 }
 
 int compare(const void* a, const void* b){
-    const char* stringa = a;
-    const char* stringb = b;
-
-    while ( *stringa != '\0' && *stringb != '\0')
-    {
-        if(*stringa < *stringb){
-            return -1;
-        }
-
-        if(*stringb < *stringa){
-            return 1;
-        }
-
-        stringa++;
-        stringb++;
-
+    if(a == NULL && b == NULL){
+        return 0;
     }
-
-    if(*stringa == '\0'){
+    if(a == NULL){
         return -1;
     }
+    if(b == NULL){
+        return 1;
+    }
+    const char* stringa = a;
+    const char* stringb = b;
+    int val; int i = 1;
 
-    if(*stringb == '\0'){
+    char tracka = *stringa; char trackb = *stringb;
+
+    while(tracka != '\0' || trackb != '\0'){
+
+        if(tracka > 90){
+            tracka-=32;
+        }
+        if(trackb > 90){
+            trackb-=32;
+        }
+
+        val = tracka - trackb;
+
+        if(val != 0){
+            return val;
+        }
+
+        tracka = stringa[i]; trackb = stringb[i];
+        i++;
+    }
+
+    if(tracka == '\0' && trackb == '\0'){
+        return 0;
+    }
+    if(tracka == '\0'){
         return 1;
     }
 
-    return 0; 
+    return -1;
+
 }
 
 int fold(int* tab, size_t nel, int start, int (*func)(int a, int b)){
@@ -108,13 +140,9 @@ int doubling(int a){
 
 int main(int argc, char* argv[]){
 
-    int arr[] = {1,2,3,4,5};
+    char* a = "Laura"; char* b = "laur";
+    printf("%d\n", compare(a, b));
 
-    printf("%d\n", fold(arr, 5, 0, &add));
-
-    int* newArr =  map(arr, 5, &doubling);
-
-    printTab(newArr, 5);
-
-    return EXIT_SUCCESS;
+    a = "HellO"; b = "heLlo";
+    printf("%d\n", compare(a, b));
 }
