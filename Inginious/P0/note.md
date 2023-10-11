5157f1408a53ce96985670ea12981cd7067b8f6d
Tfloow <88899420+Tfloow@users.noreply.github.com>
2023-10-11 14:57:35 +0200

    Init and github action

--- Inginious/P0/note.md
+++ Inginious/P0/note.md
@@ -5,6 +5,7 @@
     - [Free](#free)
     - [Malloc](#malloc)
   - [How to Build it](#how-to-build-it)
+  - [Implementing](#implementing)
 
 
 In this project we are tasked with building an equivalent to `malloc` and `free`. We will mimic the heap using an array of 64000 bytes (64 kbytes). This work is heavily inspired by the [lesson](https://moodle.uclouvain.be/pluginfile.php/237345/mod_resource/content/5/03-gestion_memoire.pdf) about malloc and free.
@@ -34,3 +35,8 @@ Important elements of a good Malloc function are listed here:
 ## How to Build it
 
 We will implement those functions like a double linked list. We are making each empty space in the heap like a possible payload for data. This way we use more meta data (especially if they are a lot of small spot). Like this we can use a simple algorithm implementing a first fit and next fit. By combining the both of them we can ensure a quick and powerful read while maintaining a good access to data and good locality.
+
+## Implementing
+
+![Fonctionnement](fonctionnement.png)
+
--- Inginious/P0/project.c
+++ Inginious/P0/project.c
@@ -2,10 +2,35 @@
 #include <stddef.h>
 
 // Vous n'utiliserez pas le vrai heap mais un tableau accessible globalement déjà déclaré de la manière suivante:
-uint8_t MY_HEAP[64000];
+uint8_t MY_HEAP[64000]; // max of 8 is 256 numbers
+
+uint16_t cast(uint8_t a, uint8_t b){
+    /*
+    Cast 2 digits together to have a 16 bits number
+    */
+    uint16_t res = a << 8;
+    res += b;
+    return res;
+}
+
+void write16(uint8_t* arr, uint16_t index, uint16_t value){
+    // Write in a 8 bits array 16 bits number. We write on 2 bits
+    // Values is capped at 64 000 of size.
+    if(index > 63998){
+        printf(" [LOG]: Not enough space to write\n");
+    }
+    arr[index] = (value & 65280) >> 8;
+    arr[index + 1] = (value & 255);
+}
 
 void init(){
     // Not compulsory just to prepare our fake heap
+    static first = 2; // the first value are used for the pointer
+    write16(MY_HEAP, 0, 2);
+    printf("%d\n", cast(MY_HEAP[0], MY_HEAP[1])); // to check we have correctly allocate the space
+    write16(MY_HEAP, 2, 63996); // Write the size left
+    write16(MY_HEAP, 4, 0); // 0 address is for the variable first so it's our type of null
+
 }
 
 void my_free(void *pointer) {
