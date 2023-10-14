e133046634a4c2aae063bd86ec820b403e3f4069
Tfloow <88899420+Tfloow@users.noreply.github.com>
2023-10-14 23:07:57 +0200

    Added the Tex report + added comments, improved code and checked the code

--- Inginious/P0/project.c
+++ Inginious/P0/project.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include<math.h>
+#include <math.h>
 #include <stdint.h>
-
-
 
 uint8_t MY_HEAP[64000]; // The heap where we allocate data
 
+// Write Function
 void write_wordtobytes(uint16_t nbr, uint8_t *a, uint8_t *b){
     /*
     Function to write 16 bits word on 2 bytes 
@@ -19,21 +18,6 @@ void write_wordtobytes(uint16_t nbr, uint8_t *a, uint8_t *b){
 
     *a = nbr >> 8; //high order bits
     *b = nbr; //low order bits
-}
-
-uint16_t read_bytestoword(uint8_t a, uint8_t b){
-    /*
-    Function that reads 2 bits and return the result as a 16 bits
-
-    a: value that represents the higher weight bit
-    b: value that represents the lower order bit
-    
-    return: 16 bits data from a and b
-    */
-    
-    uint16_t res = a << 8;
-    res += b;
-    return res;
 }
 
 void write_heap(uint8_t* arr, uint16_t index, uint16_t value){
@@ -51,11 +35,55 @@ void write_heap(uint8_t* arr, uint16_t index, uint16_t value){
     arr[index + 1] = b;
 }
 
+uint16_t read_bytestoword(uint8_t a, uint8_t b){
+    /*
+    Function that reads 2 bits and return the result as a 16 bits
+
+    a: value that represents the higher weight bit
+    b: value that represents the lower order bit
+    
+    return: 16 bits data from a and b
+    */
+    
+    uint16_t res = a << 8;
+    res += b;
+    return res;
+}
+
+uint16_t isFree(uint8_t* arr, uint16_t index){
+    /*
+    Check if a spot is free in the heap
+
+    arr:   the HEAP
+    index: the index with the meta data
+
+    return: 0 if NOT free
+    */
+    uint16_t meta = read_bytestoword(arr[index], arr[index+1]);
+    return !(meta % 2);
+}
+
+// Debug function
+void dbgprint(uint8_t* arr, uint16_t amount){
+    /*
+    Print the first AMOUNT elements of the ARR
+
+    arr:    the HEAP
+    amount: the amount we want to see
+    */
+    printf("[ ");
+    for(int i = 0; i < amount; i++){
+        printf("%d, ", arr[i]);
+    }
+    printf("... ]\n");
+}
+
 void init(){
     /**
     heap initialization: all bytes are free. 2 words of metadata on each side
     */
 
+    // We use ODD NUMBERS because it shows if the upcoming bits are free or unoccupied
     write_heap(MY_HEAP, 0, 63996);
     write_heap(MY_HEAP, 63998, 63996);
 
@@ -71,7 +99,7 @@ void* my_malloc(size_t size){
     */
 
     if(size>= 63996){
-        printf("Requested size is too big");
+        printf("Requested size is too big\n");
         return NULL;
     }
 
@@ -82,7 +110,7 @@ void* my_malloc(size_t size){
 
     int i = 0;
 
-    uint16_t h = read_bytestoword(MY_HEAP[i], MY_HEAP[i+1]);
+    uint16_t h;
 
     /*
     Searches for big enough block. 3 cases to check
@@ -91,15 +119,23 @@ void* my_malloc(size_t size){
     -block already allocated
     */
 
-    while( i < 63998 && ( h < searched_size || ((h && 0x1) !=0) )){
-        i = i + h + 4;
+    // Need to recheck if the loop is correct or not
+    while( i < 63996 && ( h < searched_size || ((h && 0x1) !=0) )){
         h = read_bytestoword(MY_HEAP[i], MY_HEAP[i+1]);
+        if(isFree(MY_HEAP, i) && (h > searched_size+4)){
+            printf("Found space\n");
+            break;
+        }
+        i = i + h + 4;
     }
 
     //end of MY_HEAP without finding a block:
-    if(i >= 63998){printf("End of MY_HEAP reached: couldn't allocate memory");}
+    if(i >= 63998){
+        printf("End of MY_HEAP reached: couldn't allocate memory\n");
+        return NULL;
+    }
 
-    else{//stops at first big enough block, must check if block has to be divided then writes it as allocated (LOB = 1)
+    //stops at first big enough block, must check if block has to be divided then writes it as allocated (LOB = 1)
 
     if(h > searched_size + 4){ //+4 for extra metadatas from splitting
 
@@ -108,20 +144,20 @@ void* my_malloc(size_t size){
         write_heap(MY_HEAP, i, searched_size + 1 - 4); //+1 : is allocated, -4 subtracts extra metadatas size
         
         write_heap(MY_HEAP, i + searched_size, leftover-4);
+        write_heap(MY_HEAP, i + searched_size - 2, searched_size + 1 - 4);
 
-    }
-
-    else{write_heap(MY_HEAP, i, searched_size + 1 - 4);} //block fits perfectly
-
-    }
+    }else{
+        write_heap(MY_HEAP, i, searched_size + 1 - 4); //block fits perfectly
+        write_heap(MY_HEAP, i + searched_size - 2, searched_size + 1 - 4);
+    } 
 }
 
 
-void my_free(void *pointer) {
+void my_free(void *ptr) {
     /*
     Free space that was previously allocated by my_malloc function
 
-    pointer: a pointer to the area in the HEAP where we have allocated some data that we want now to free
+    ptr: a pointer to the area in the HEAP where we have allocated some data that we want now to free
     */
     // Our free
 }
@@ -135,7 +171,8 @@ int main(int argc, char *argv[]){
     uint16_t size = read_bytestoword(MY_HEAP[63998], MY_HEAP[63999]);
 
     printf("%d\n", size);
-
+    void* test = my_malloc(4);
+    dbgprint(MY_HEAP, 20);
 
 
     return 0;
